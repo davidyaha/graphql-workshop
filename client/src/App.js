d2efa9df812eca7247cd81a7ac92eaaeefac24e3
David Yahalomi <davidyaha@gmail.com>
2016-11-29 17:21:42 +0200

    Step 3.0 - Writing react app on that uses our mocked API

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <h2>Github Follow Manager</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
