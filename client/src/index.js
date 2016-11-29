a9e5f68b16708728b4bbc7004c166bf761e09b29
David Yahalomi <davidyaha@gmail.com>
2016-11-29 18:05:48 +0200

    Step 3.6 - Connecting to our graphql API server

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { ApolloClient, createNetworkInterface } from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
 import App from './App';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 
+const networkInterface = createNetworkInterface({ uri: '/graphql' });
+const client = new ApolloClient({ networkInterface });
+
 const app = (
-  <App/>
+  <ApolloProvider client={client}>
+    <App/>
+  </ApolloProvider>
 );
 ReactDOM.render(
   app,
